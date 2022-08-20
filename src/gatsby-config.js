32cf4079aaf19771bd7ad1dcd3c6c867a047ceb8
xylnx <xylnx@mailbox.org>
2022-08-20 20:15:50 +0200

    Remove redundant config file

--- src/gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  /* Your site config here */
-  plugins: ["gatsby-plugin-sass"],
-  siteMetadata: {
-    title: "Kokedami Mamy",
-    description: "the best kokedamas in town",
-    copyright: "Kokedami Mamy 2022",
-  },
-}
