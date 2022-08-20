fe641fde73c5cab56f3a3b108434060698e9e2c5
xylnx <xylnx@mailbox.org>
2022-08-20 20:15:17 +0200

    Add metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,9 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
   plugins: ["gatsby-plugin-sass"],
+  siteMetadata: {
+    title: "Kokedami Mamy",
+    description: "the best kokedamas in town",
+    copyright: "Kokedami Mamy 2022",
+  },
 }
