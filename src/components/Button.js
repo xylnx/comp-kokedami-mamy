b025bcce905958b651c33a6f17a1560410a8e91d
xylnx <xylnx@mailbox.org>
2022-08-23 19:47:39 +0200

    Add a component for buttons

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import "../assets/scss/buttons.scss"
+
+const Button = () => {
+  return (
+    <a
+      href="https://eshop.kokedamimamy.cz/"
+      target="_blank"
+      rel="noreferrer"
+      class="btn"
+    >
+      <span class="btn__text">e-shop</span>
+    </a>
+  )
+}
+
+export default Button
