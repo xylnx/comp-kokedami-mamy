55d8a677b649fdd40b79237ef724a5e97df5443e
xylnx <xylnx@mailbox.org>
2022-08-23 19:56:48 +0200

    Query data and display it on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,43 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
+// Components
 import Layout from "../components/Layout"
+import Button from "../components/Button"
 
-export default function Home() {
+export default function Home({ data }) {
+  const image = getImage(data.markdownRemark.frontmatter.featured_image)
+  const content = data.markdownRemark.html
+
   return (
-    <Layout>
-      <p>Hello World</p>
+    <Layout image={image}>
+      <section
+        id="about"
+        className="content flow"
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+      <Button />
     </Layout>
   )
 }
+
+export const pageQuery = graphql`
+  query homePageQuery {
+    markdownRemark(fileAbsolutePath: { regex: "/home.md/" }) {
+      frontmatter {
+        slug
+        title
+        featured_image {
+          childImageSharp {
+            gatsbyImageData(
+              placeholder: BLURRED
+              formats: [AUTO, WEBP, AVIF, JPG]
+            )
+          }
+        }
+      }
+      html
+    }
+  }
+`
