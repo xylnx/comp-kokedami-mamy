b357609a512baf44fff2b6e5c8c2230ea5858d28
xylnx <xylnx@mailbox.org>
2022-08-26 23:39:46 +0200

    Add custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+// Components
+import Layout from "../components/Layout"
+import Button from "../components/Button"
+
+const custom404 = () => {
+  return (
+    <Layout>
+      <h1>Ooops!</h1>
+      <p>Sorry, we could not find any content at this location.</p>
+    </Layout>
+  )
+}
+
+export default custom404
