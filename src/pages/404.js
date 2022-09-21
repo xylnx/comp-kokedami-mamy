15f950952e29f494cf1508bbcf95865b55e21a3e
xylnx <xylnx@mailbox.org>
2022-09-21 06:09:00 +0200

    Link back to home page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from "react"
 
 // Components
 import Layout from "../components/Layout"
+import Button from "../components/Button"
 import { Seo } from "../components/Seo"
 
 const custom404 = () => {
@@ -9,6 +10,7 @@ const custom404 = () => {
     <Layout>
       <h1 style={{ marginTop: ".8rem" }}>Ooops!</h1>
       <p>Sorry, we could not find any content at this location.</p>
+      <Button title="Go to Home page" href="/" isBlank={false} />
     </Layout>
   )
 }
