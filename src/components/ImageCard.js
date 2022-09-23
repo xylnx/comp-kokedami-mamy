6167bf8aef26b0bbd7fcc75cac63173d56f68cf8
xylnx <xylnx@mailbox.org>
2022-09-23 11:36:46 +0200

    ImgCard: use css module

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,33 +1,15 @@
+// Gatsby
 import React from "react"
 import Img from "gatsby-image"
 
-const ImageCard = ({ image }) => {
-  const styles = {
-    imgCard: {
-      position: "relative",
-      width: "100%",
-      maxWidth: "40rem",
-      height: "auto",
-    },
-    img: {},
-    imgBg: {
-      position: "absolute",
-      width: "100%",
-      height: "100%",
-      top: "-4px",
-      left: "4px",
-      border: "2.5px solid #007229",
-      transform: "rotate(2deg)",
-    },
-  }
+// Styles
+import * as styles from "../assets/scss/ImageCard.module.scss"
 
+const ImageCard = ({ image }) => {
   return (
-    <div className="img-card" style={styles.imgCard}>
-      <Img fluid={image} alt="" className="img-card__img" style={styles.img} />
-      <div
-        style={styles.imgBg}
-        className="img-card__background img-card__background--ws-2"
-      ></div>
+    <div className={styles.imgCard}>
+      <Img fluid={image} alt="" />
+      <div className={styles.imgBg}></div>
     </div>
   )
 }
